782c3c0eb9b9b291f9ef4bd2bebd6ac426828fdd
conorreid <conorreid1995@hotmail.com>
2019-02-19 16:43:37 +0000

    Created and implemented oscillator script to move obstacle in level

---
+++ Assets/Scenes/Oscillator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    float movementFactor;
+    [SerializeField] float period = 3f;
+    Vector3 startingPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / period; //grows from zero counting cycles
+
+        const float tau = Mathf.PI * 2;
+        float rawSineWave = Mathf.Sin(cycles * tau);
+        movementFactor = (rawSineWave / 2f) + 0.5f;
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+        
+    }
+}
